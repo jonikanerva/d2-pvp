688b4282f2aa2aeb14c18dc41b0058379510138e
Joni Kanerva <jhonny@jhonny.org>
2023-08-13 10:49:22 +0300

    better ka info

--- src/Activities.tsx
+++ src/Activities.tsx
@@ -80,11 +80,9 @@ const Activities: React.FC<ActivitiesProps> = ({
           <tr>
             <th>Date</th>
             <th>Mode</th>
-            <th className="alignCenter">KA</th>
             <th className="alignCenter">K</th>
-            <th className="alignCenter">A</th>
             <th className="alignCenter">D</th>
-            <th className="alignCenter">KA/D</th>
+            <th className="alignCenter">K/D</th>
             <th className="alignCenter">O</th>
           </tr>
         </thead>
@@ -94,14 +92,12 @@ const Activities: React.FC<ActivitiesProps> = ({
               <tr key={key}>
                 <td>{dateFormat(activity.period)}</td>
                 <td>{modeName(activity.activityDetails.mode.toString())}</td>
-                <td className="alignCenter">
-                  {activity.values.opponentsDefeated.basic.displayValue}
-                </td>
-                <td className="alignCenter">
-                  {activity.values.kills.basic.displayValue}
-                </td>
-                <td className="alignCenter">
-                  {activity.values.assists.basic.displayValue}
+                <td className="alignCenter noWrap">
+                  {activity.values.opponentsDefeated.basic.displayValue}{' '}
+                  <span className="detailInfo">
+                    ({activity.values.kills.basic.displayValue}+
+                    {activity.values.assists.basic.displayValue})
+                  </span>
                 </td>
                 <td className="alignCenter">
                   {activity.values.deaths.basic.displayValue}
